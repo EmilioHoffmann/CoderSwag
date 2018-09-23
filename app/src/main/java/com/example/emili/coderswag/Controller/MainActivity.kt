dc2d794f2f084efe341b1723e12259223a9cc9d3
EmilioHoffmann <emiliohoffmann@hotmail.com>
2018-09-23 20:38:25 -0300

    Bad category adapter for the listView

--- app/src/main/java/com/example/emili/coderswag/Controller/MainActivity.kt
+++ app/src/main/java/com/example/emili/coderswag/Controller/MainActivity.kt
@@ -1,22 +1,23 @@
 package com.example.emili.coderswag.Controller
 
-import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.ArrayAdapter
-import com.example.emili.coderswag.Model.Category
+import android.support.v7.app.AppCompatActivity
+import com.example.emili.coderswag.Adapters.CategoryAdapter
 import com.example.emili.coderswag.R
 import com.example.emili.coderswag.Services.DataService
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
-    lateinit var adapter: ArrayAdapter<Category>
+    lateinit var adapter: CategoryAdapter
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        adapter = ArrayAdapter(this, android.R.layout.simple_list_item_1, DataService.categories)
+        adapter = CategoryAdapter(this, DataService.categories)
         categoriesListView.adapter = adapter
+
+
     }
 }
